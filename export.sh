71363dadc4dc7f4c0b0bd88693578c4bc3779de2
dos-kid <52602144+dos-kid@users.noreply.github.com>
2019-11-21 17:14:09 +0530

    Delete export.sh

--- export.sh
+++
@@ -1,4 +0,0 @@
-export FLASK_APP="application.py"
-export FLASK_DEBUG=1
-export DATABASE_URL="postgres://lgblsznbvphfrp:1421cd0e3f11a3d472a215008ae23f015cc7864ecbf615ab47c499c62ec102fe@ec2-23-23-92-204.compute-1.amazonaws.com:5432/d3osb2va95ounc"
-flask run
